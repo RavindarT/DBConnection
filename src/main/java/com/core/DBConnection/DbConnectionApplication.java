f538934f3536235870b1f133cefcb9b3b767c50c
Ravindar Thirugnanasambandam <ravindar.t.j2ee@gmail.com>
2020-05-10 11:34:47 -0400

    committed

--- src/main/java/com/core/DBConnection/DbConnectionApplication.java
+++ src/main/java/com/core/DBConnection/DbConnectionApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DbConnectionApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Check");
 		SpringApplication.run(DbConnectionApplication.class, args);
 	}
 
